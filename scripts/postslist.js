62a5614f106489082e9f1e8362fd481c387d95bf
thomasv <thomasv@vmware.com>
2021-04-05 19:38:54 +0530

    Adding comments

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -3,14 +3,14 @@ var deletePost = document.getElementById("deletePostModal");
 var cancelDelete = document.getElementById("cancel");
 var confirmDelete = document.getElementById("confirm");
 
-
+//Method to delete modal.
 function deleteModal(post){
     openModel(deletePost);
     deletePost.addEventListener("click",function(event){
         if(event.target == cancelDelete ){
-            console.log("cancel detected");
             closeModel(deletePost);
         }
+        //when the delete icon is clicked for a particular post, then it is deleted if the user confirms.The onclick event of the post triggers this method to be called
         if(event.target == confirmDelete){
             var delelement =  document.getElementById(post);
             try{
@@ -30,7 +30,7 @@ window.addEventListener("click", function(event) {
     }
 });
 
-
+//This method sets the author, title and content into the session storage so that when the post is expanded the details are dynamically filled in the post.html page
 function viewPost(author, title, content){
 
     authorText = document.getElementById(author).innerText;
